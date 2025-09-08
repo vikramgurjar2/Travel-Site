fe1b07f539eadbb5d6836d4df06d3bb13778dd97
vikram gurjar <vikramgurjar9179@gmail.com>
2025-09-08 22:50:57 +0530

    changes done

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  css: {
+    postcss: "./postcss.config.cjs",
+  },
 });
